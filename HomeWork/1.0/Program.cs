b142fb0a17fc5f20e5c34bf3809e49153dd1cd65
Alexey <lex1645@yandex.ru>
2022-11-25 21:20:11 +0300

    Перевое задание ДЗ

---
+++ HomeWork/1.0/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+
+Console.Write("Введите число 1: ");
+string a = Console.ReadLine();
+int num1 = int.Parse(a);
+Console.Write("Введите число 2: ");
+string b = Console.ReadLine();
+int num2 = int.Parse(b);
+
+int max = num1;
+
+if(num2 > max) max = num2;
+
+Console.Write("max = ");
+Console.WriteLine(max);
