8b6dec6f0b89438460a75f5725b9bf532413dc87
Alexey <lex1645@yandex.ru>
2022-11-25 22:03:58 +0300

    Исправил 1.0 задание

--- HomeWork/2.0/Program.cs
+++ HomeWork/2.0/Program.cs
@@ -1,8 +1,4 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
-// какое число большее, а какое меньшее.
-
-
-Console.Write("Введите число 1: ");
+﻿Console.Write("Введите число 1: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите число 2: ");
 int num2 = int.Parse(Console.ReadLine());
