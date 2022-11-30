6e9aafceb96168257af14b49b58621be34688fe1
Alexey <lex1645@yandex.ru>
2022-11-30 19:35:26 +0300

    Task 2.3.0

---
+++ HomeWork 2.0/2.0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+
+int Num(int num)
+{
+    Console.WriteLine(num);
+    return num % 100 /10;
+}
+
+Console.WriteLine(Num(new Random().Next(100,1000)));
