19435eddec1229d58706addadcf1dea172c295c2
Alexey <lex1645@yandex.ru>
2022-11-30 23:53:11 +0300

    HomeWork 2.3.0

--- HomeWork 2.0/3.0/Program.cs
+++ HomeWork 2.0/3.0/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+string Num(int num)
+{
+    if (num < 99)
+        return "Третьей цифры нет";
+
+    if (num > 999)
+        return "Erorr";
+
+    else 
+        return $"Третья цифра - {num % 10}";
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Num(num));
