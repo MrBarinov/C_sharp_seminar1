a1b86d28229f0c7b6168710b483aad6f1840026e
Alexey <lex1645@yandex.ru>
2022-11-30 23:05:07 +0300

    HomeWork 2.1.0

---
+++ HomeWork 2.0/1.0/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+string Num(int num)
+
+{
+    if (num < 1000)
+        return $"Второе число = {num / 10 % 10}";
+
+    else
+        return "Число не трехзначное - erorr";
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Num(num));
