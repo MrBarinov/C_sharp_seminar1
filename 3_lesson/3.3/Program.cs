0d41ff5a1dd38a62ef580a1f3c7caa31cfe3a598
Alexey <lex1645@yandex.ru>
2022-12-06 22:07:39 +0300

    Вторая и третья  задачи

--- 3_lesson/3.3/Program.cs
+++ 3_lesson/3.3/Program.cs
@@ -1,26 +1,22 @@
-﻿// 3. Напишите программу, которая принимает на вход число (N)
-// и выдаёт таблицу квадратов чисел от 1 до N.
+﻿void Chet(int N)
+{
+    int i = 0;
+    double pere;
+    while (i < N)
+    {
+        i += 1;
+        if (i == (N))
+        {
+            pere = Math.Pow(i, 2);
+            Console.Write($"{pere}");
+        }
+        else
+        {
 
-void Chet(int N)
-{
-int i = 0;
-double pere;
-while (i < N )
-{
-i += 1;
-if (i == (N))
-{
-pere = Math.Pow(i,2);
-Console.Write($"{pere}");
-}
-else
-{
-
-pere = Math.Pow(i,2);
-Console.Write($"{pere},");
-}
-}
-
+            pere = Math.Pow(i, 2);
+            Console.Write($"{pere},");
+        }
+    }
 
 }
 Console.WriteLine("введите ч");
