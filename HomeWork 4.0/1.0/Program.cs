1b7e9ff6ed66d8ee478bc3e0a1b3ed32733f923c
Alexey <lex1645@yandex.ru>
2022-12-08 21:51:29 +0300

    Первое задание по ДЗ

--- HomeWork 4.0/1.0/Program.cs
+++ HomeWork 4.0/1.0/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в 
+//натуральную степень B. Без модуля Math, используем цикл for.
+
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+int Stepen(int A, int B)
+{
+    int c = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        c = c * A;
+    }
+    return c;
+}
+
+Console.WriteLine(Stepen(a, b));
