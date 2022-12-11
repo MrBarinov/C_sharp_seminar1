531aac9054c60e1cb8a649dd72dbfca6214dc74d
Alexey <lex1645@yandex.ru>
2022-12-11 20:51:33 +0300

    Второе задание по ДЗ

--- HomeWork 4.0/2.0/Program.cs
+++ HomeWork 4.0/2.0/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает 
+//на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int Summa(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine(Summa(a));
