0f4e653025ee1af46c8c68a2b2e78322caf91e0d
Alexey <lex1645@yandex.ru>
2022-12-13 00:58:10 +0300

    Прошли  5_lesson

--- HomeWork 5.0/5.0/Program.cs
+++ HomeWork 5.0/5.0/Program.cs
@@ -2,19 +2,29 @@
 //положительными трёхзначными числами. Напишите программу, 
 //которая покажет количество чётных чисел в массиве.
 
-
-
-void MassivFill(int[] a)
+void Print(int[] arr)
 {
-    int Lenght = a.Length;
-    int i = 0;
-    while (i < Lenght)
-    {
-        a[i] = new Random().Next(100, 1000);
-        i++;
-    }
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
 }
 
+int[] Mass(int size)
+{
+    int[] arr = new int[size];
 
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(100, 1000);
+    return arr;
+}
 
+void Chet(int[] arr)
+{
+    int num = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] % 2 == 0)
+        num++;
+}
 
