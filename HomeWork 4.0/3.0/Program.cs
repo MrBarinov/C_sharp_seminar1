2f1bcaea5034ecb1bd4a73a21ffcc4158828e970
Alexey <lex1645@yandex.ru>
2022-12-12 18:02:25 +0300

    Третье задание по ДЗ

--- HomeWork 4.0/3.0/Program.cs
+++ HomeWork 4.0/3.0/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая задаёт 
+//массив из 8 элементов и выводит их на экран.
+
+
+void MassivFill(int[] a)
+{
+    int Lenght = a.Length;
+    int i = 0;
+    while (i < Lenght)
+    {
+        a[i] = new Random().Next(1, 100);
+        i++;
+    }
+}
+
+void MassivPrint(int[] c)
+{
+    int count = c.Length;
+    int pos = 0;
+    Console.Write("[ ");
+    while (pos < count)
+    {
+        Console.Write($"{c[pos]} ");
+        pos++;
+        if (pos < count)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
+
+int[] array = new int[8];
+
+MassivFill(array);
+MassivPrint(array);
