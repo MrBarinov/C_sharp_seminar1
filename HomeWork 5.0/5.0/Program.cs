229a4726f2d0058624e7499c9caae30f9bf1e51b
Alexey <lex1645@yandex.ru>
2022-12-12 19:02:25 +0300

    Смержили ветки

---
+++ HomeWork 5.0/5.0/Program.cs
@@ -0,0 +1,15 @@
+﻿//
+
+
+
+void MassivFill(int[] a)
+{
+    int Lenght = a.Length;
+    int i = 0;
+    while (i < Lenght)
+    {
+        a[i] = new Random().Next(100, 1000);
+        i++;
+    }
+}
+
