669a234fe08ff7875fdf577a9cae1bae0c4345ba
Alexey <lex1645@yandex.ru>
2022-12-17 15:19:11 +0300

    Смержились с 5 веткой

--- 6_lesson/6.0/Program.cs
+++ 6_lesson/6.0/Program.cs
@@ -1,30 +1,30 @@
 ﻿// 1. Напишите программу, которая перевернёт одномерный массив
 // (последний элемент будет на первом месте, а первый - на последнем и т.д.)
 
-void MassivFill(int[] a)
+void Print(int[] arr)
 {
-    int Lenght = a.Length;
-    int i = 0;
-    while (i < Lenght)
-    {
-        a[i] = new Random().Next(1, 100);
-        i++;
-    }
-}
-
-void MassivPrint(int[] c)
-{
-    int count = c.Length;
-    int pos = 0;
+    int size = arr.Length;
     Console.Write("[");
-    while (pos < count)
+    for (int i = 0; i < size; i++)
     {
-        Console.Write($"{c[pos]}");
-        pos++;
-        if (pos < count)
+        Console.Write($"{arr[i]}");
+        if (i < size - 1)
         {
-            Console.Write(", ");
+            Console.Write("; ");
         }
     }
     Console.Write("]");
 }
+
+int[] Mass(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(10, 100);
+    }
+    return arr;
+}
+
+int[] arr_1 = Mass(int.Parse(Console.ReadLine()));
+Print(arr_1);
