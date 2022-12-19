da52fee0e2799a67b86a9d0b9de5ec6c2dab4c02
Alexey <lex1645@yandex.ru>
2022-12-19 23:08:43 +0300

    HomeWork 9 lesson

---
+++ 3_lesson/3.3/Program 2.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая
+//принимает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N
+
+void Square(int N)
+{
+    int i = 0;
+    double num;
+    while (i < N)
+    {
+        i += 1;
+        if (i == (N))
+        {
+            num = Math.Pow(i, 2);
+            Console.Write($"{num}");
+        }
+        else
+        {
+            num = Math.Pow(i, 2);
+            Console.Write($"{num},");
+        }
+    }
+}
+Console.WriteLine("введите ч");
+int chislo = int.Parse(Console.ReadLine());
+Square(chislo);
