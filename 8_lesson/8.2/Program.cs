2edb8cd6cbd20b8aa12160ef66d86c7d00b68e42
Alexey <lex1645@yandex.ru>
2022-12-27 12:28:02 +0300

    Task 3 in lesson 8

--- 8_lesson/8.2/Program.cs
+++ 8_lesson/8.2/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Составить частотный словарь элементов двумерного массива.
+// Частотный словарь содержит информацию о том, сколько раз
+// встречается элемент входных данных. Значения элементов массива 0..9
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] MassNums(int row, int column)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(1, 10);
+
+    return arr;
+}
+
+void PrintMass(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.WriteLine($"{i} meets: {arr[i]}");
+    Console.WriteLine();
+}
+
+int[] FrequencyDict(int[,] arr)
+{
+    int[] freq = new int[10];
+
+    foreach (int item in arr)
+        freq[item] += 1;
+
+    return freq;
+}
+Console.Write("Введите колличество строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите колличество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column);
+Print(arr_1);
+
+int[] mass = FrequencyDict(arr_1);
+PrintMass(mass);
